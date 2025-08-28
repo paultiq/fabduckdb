80a0e0f976bf0a95a009ae6f99f3169f4e6451ea
paul@iqmo.com <paul@iqmo.com>
2025-08-28 18:07:12 -0400

    Reworking this project

--- fabduckdb/_version.py
+++ fabduckdb/_version.py
@@ -1,8 +1,6 @@
-# This file is entirely overwritten at build time
-# Module dunamai is used to create the dev and release __version__'s
-# from the most recent git tag
-# See git action yaml for details
+import importlib.metadata
 
-__version__ = "0.0.0"
-__commit__ = "1234567890"
-__commit_short__ = "1234"
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
